4a30ddcac2cae2e875dbd9befa25bcfa7a9f0a69
Nishant Sharma <2025ag05585@wilp.bits-pilani.ac.in>
2026-07-15 16:02:54 +0530

    Add files via upload

---
+++ 1stprog.py
@@ -0,0 +1,14 @@
+print("hello World!")
+print("hello World!"*5)
+a = 5
+b = 7
+c = a+b
+print("c =",c)
+d='x'
+e='y'
+print(d+e)# string concatenation
+f= "Hello"
+g= "World"
+l = f+""+g
+print(l)# string concatenation
+
